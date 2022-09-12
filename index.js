f8d855409871a5336187d0573d4c4c12dc0a5d36
Dragoș Străinu <str.dr4605@gmail.com>
2022-09-12 19:59:40 +0300

    add some react components

--- index.js
+++ index.js
@@ -7,15 +7,48 @@ class LikeButton extends React.Component {
     this.state = { liked: false };
   }
 
+  handleOnClick = () => {
+    console.log("clicked");
+    this.setState({ liked: true });
+  }
+
   render() {
     if (this.state.liked) {
       return "You liked this.";
     }
 
-    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+    return (
+      <button onClick={this.handleOnClick}>
+        Like <Hello />
+      </button>
+    );
   }
 }
 
-const domContainer = document.querySelector("#like_button_container");
+function Container() {
+  return (
+    <main id="main-container">
+      Container
+      <LikeButton />
+      <Hello></Hello>
+    </main>
+  );
+}
+
+function Hello() {
+  return <span>Hello</span>;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My Website</h1>
+      <LikeButton></LikeButton>
+      <Container></Container>
+    </div>
+  );
+}
+
+const domContainer = document.querySelector("#root");
 const root = ReactDOM.createRoot(domContainer);
-root.render(<LikeButton />);
+root.render(<App />);
