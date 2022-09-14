97dc5fa2b9abe5a31ef5043e2233459ee2101581
Dragoș Străinu <str.dr4605@gmail.com>
2022-09-14 18:42:21 +0300

    Add components from lesson1

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,9 +1,56 @@
+import React from 'react';
 import './App.css';
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  handleOnClick = () => {
+    console.log("clicked");
+    this.setState({ liked: true });
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+
+    return (
+      <button onClick={this.handleOnClick}>
+        Like
+      </button>
+    );
+  }
+}
+
+function Header() {
+  return <h2>Section with list</h2>;
+}
+
+function Section() {
+
+  return (
+  <section>
+    <Header/>
+    <ul>
+      <li>One</li>
+      <li>Doi</li>
+      <li>Three</li>
+      <li><LikeButton/></li>
+    </ul>
+  </section>
+  );
+}
 
 function App() {
   return (
     <div>
-      Hello react
+      <h1>Hello to my Website</h1>
+      <LikeButton />
+      <Section />
+      <Section />
     </div>
   );
 }
