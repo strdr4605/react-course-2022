18c5bdff19180d3d29e4ec5b59337d51c45d2c48
Dragoș Străinu <str.dr4605@gmail.com>
2022-09-16 13:06:43 +0300

    lesson2 add code

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -25,34 +25,73 @@ class LikeButton extends React.Component {
   }
 }
 
-function Header() {
-  return <h2>Section with list</h2>;
+function Header(props) {
+  const {title} = props;
+  return <h2>Section with list, {title}</h2>;
 }
 
-function Section() {
+function Section(props) {
+  const {bgClass, title, id, desc, listColor} = props;
 
   return (
-  <section>
-    <Header/>
-    <ul>
+  <section id={id} className={bgClass}>
+    <Header title={title}/>
+    <ul style={{color: listColor}}>
       <li>One</li>
       <li>Doi</li>
       <li>Three</li>
       <li><LikeButton/></li>
     </ul>
+    <div>{desc}</div>
   </section>
   );
 }
 
 function App() {
+  const description = "My awesome description";
+  const color1 = "magenta";
+  const color2 = "yellow";
+
   return (
-    <div>
+    <div className='app'>
       <h1>Hello to my Website</h1>
       <LikeButton />
-      <Section />
-      <Section />
+      <Section 
+        desc={description} 
+        listColor={color1}
+        id="s1" 
+        title="Section1" 
+        bgClass="blue" 
+      />
+      <Section 
+        desc={description} 
+        listColor={color2}
+        id="s2" 
+        title="Sectiunea a 2" 
+        bgClass="green" 
+      />
     </div>
   );
 }
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+
+const { a } = obj;
+
+
+const myVar = obj.a
+const b = obj.b
+const c = obj.c
+
+const array = [1, 2];
+
+const [primu, aldoilea] = array;
+
+const first = array[0];
+const second = array[1];
 
 export default App;
