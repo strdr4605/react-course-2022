b589a9a6cf50a7b215dd2a989e62169004074bfd
Dragoș Străinu <str.dr4605@gmail.com>
2022-10-04 11:29:00 +0300

    lesson6 fetch user

--- lesson6/src/App.js
+++ lesson6/src/App.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import User from './components/User';
 
 function App() {
+  const [user, setUser] = useState(undefined)
 
-
+  useEffect(() => {
+    fetch('https://reqres.in/api/users/1')
+      .then(response => response.json())
+      .then(userFromAPI => {
+        console.log({userFromAPI})
+        setUser(userFromAPI.data)
+      })
+  }, []);
+  
   return (
     <div className="App">
       <h1>Lesson 6</h1>
+      <User user={user} />
     </div>
   );
 }
